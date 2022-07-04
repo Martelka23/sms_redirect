46adf7c839f52b862665c18e6fcafc8a51b6e4dc
Антон <akrik@MacBook-Air-Anton.local>
2022-07-04 12:38:46 +0300

    add msges

--- mobile/app/src/main/java/com/example/sms_redirect/MainActivity.java
+++ mobile/app/src/main/java/com/example/sms_redirect/MainActivity.java
@@ -5,12 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    public void getMessages(View view) {
+        System.out.println("Ok");
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        if (cursor.moveToFirst()) { // must check the result to prevent exception
+            do {
+                String msgData = "";
+                for(int idx=0;idx<cursor.getColumnCount();idx++)
+                {
+                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
+                }
+                // use msgData
+                System.out.println(msgData);
+            } while (cursor.moveToNext());
+        } else {
+            // empty box, no SMS
+            System.out.println("No messages");
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
